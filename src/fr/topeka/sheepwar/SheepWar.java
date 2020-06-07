f1774ca7f35d33314e7c5faa8562b3ab4d0e99de
Quentin Legot <quentin3157.github@gmail.com>
2020-06-07 18:28:38 +0200

    adding possibility to save arena as schematic

--- src/fr/topeka/sheepwar/SheepWar.java
+++ src/fr/topeka/sheepwar/SheepWar.java
@@ -3,23 +3,31 @@ package fr.topeka.sheepwar;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
 
 import fr.topeka.sheepwar.arena.Arena;
 import fr.topeka.sheepwar.commands.Commandhandler;
 import fr.topeka.sheepwar.listener.PlayerJoinLeaveListener;
 
+/**
+ * 
+ * @author Topeka_
+ */
 public class SheepWar extends JavaPlugin{
 	
 	public List<Player> _playerOnLobby = new ArrayList<>();
 	public List<Arena> _arenaList = new ArrayList<>();
 	private SheepWar _instance = this;
-	
+	public WorldEditPlugin WE;
 	
 	@Override
 	public void onEnable() {
+		WE = (WorldEditPlugin) Bukkit.getPluginManager().getPlugin("WorldEdit");
 		getCommand("sheepwar").setExecutor(new Commandhandler(_instance));
 		List<String> aliases = new ArrayList<>();
 		aliases.add("sw");
--- src/fr/topeka/sheepwar/commands/admin/CommandAdminSchem.java
+++ src/fr/topeka/sheepwar/commands/admin/CommandAdminSchem.java
@@ -1,11 +1,31 @@
 package fr.topeka.sheepwar.commands.admin;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import org.bukkit.entity.Player;
 
+import com.sk89q.worldedit.EditSession;
+import com.sk89q.worldedit.WorldEdit;
+import com.sk89q.worldedit.WorldEditException;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.extent.clipboard.BlockArrayClipboard;
+import com.sk89q.worldedit.extent.clipboard.io.BuiltInClipboardFormat;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardWriter;
+import com.sk89q.worldedit.function.operation.ForwardExtentCopy;
+import com.sk89q.worldedit.function.operation.Operations;
+import com.sk89q.worldedit.regions.CuboidRegion;
+import com.sk89q.worldedit.regions.Region;
+
 import fr.topeka.sheepwar.SheepWar;
+import fr.topeka.sheepwar.arena.Arena;
 import fr.topeka.sheepwar.commands.AbstractCommand;
 import fr.topeka.sheepwar.commands.Permission;
-
+/**
+ * 
+ * @see https://worldedit.enginehub.org/en/latest/api/examples/clipboard/
+ */
 public class CommandAdminSchem extends AbstractCommand {
 
 	public CommandAdminSchem(SheepWar instance, Player player, String label, String[] args, int nArgs) {
@@ -16,7 +36,28 @@ public class CommandAdminSchem extends AbstractCommand {
 	@Permission(_permission = "sheepwar.admin.schem")
 	@Override
 	public boolean handle() {
-		// TODO Auto-generated method stub
+		if(nArgs > 2) {
+			for(Arena a : _instance._arenaList) {
+				if(args[2].equalsIgnoreCase(a._Name)) {
+					try {
+						Region selection = _instance.WE.getSession(player).getSelection(BukkitAdapter.adapt(player.getWorld()));
+						if(selection instanceof CuboidRegion) {
+							CuboidRegion region = (CuboidRegion) selection;
+							BlockArrayClipboard clipboard = new BlockArrayClipboard(region);
+							EditSession editSession = WorldEdit.getInstance().getEditSessionFactory().getEditSession(BukkitAdapter.adapt(player.getWorld()), -1);
+								ForwardExtentCopy forwardextendcopy = new ForwardExtentCopy(editSession, region, clipboard, region.getMinimumPoint());
+								Operations.complete(forwardextendcopy);
+								File file = new File(_instance.getDataFolder() + File.separator + a._Name + ".schem");
+								ClipboardWriter writer = BuiltInClipboardFormat.SPONGE_SCHEMATIC.getWriter(new FileOutputStream(file));
+								writer.write(clipboard);			
+						}
+					} catch (WorldEditException | IOException e) {
+						_instance.getLogger().warning(e.getMessage());
+						e.printStackTrace();
+					}
+				}
+			}
+		}
 		return false;
 	}
 
