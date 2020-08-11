d909081232463dc38cfee26d6f21a79a4dcbadaa
Quentin Legot <quentin3157.github@gmail.com>
2020-08-11 17:19:48 +0200

    Fixed many bugs

--- src/main/java/fr/topeka/sheepwar/listener/DamageListener.java
+++ src/main/java/fr/topeka/sheepwar/listener/DamageListener.java
@@ -2,8 +2,11 @@ package fr.topeka.sheepwar.listener;
 
 import fr.topeka.sheepwar.arena.Arena;
 
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Egg;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -13,7 +16,7 @@ import fr.topeka.sheepwar.SheepWar;
 
 public class DamageListener implements Listener{
 
-	@EventHandler(ignoreCancelled = true)
+	@EventHandler
 	public void onDamage(EntityDamageEvent event) {
 		if(event.getEntity() instanceof Player) {
 			SheepWar instance = SheepWar.getInstance();
@@ -32,13 +35,50 @@ public class DamageListener implements Listener{
 	}
 	
 	// also pve
-	@EventHandler(ignoreCancelled = true)
+	@EventHandler
 	public void onPvp(EntityDamageByEntityEvent event) {
 		Entity damagerEntity = event.getDamager();
 		if(event.getEntity() instanceof Player) {
 			Player victim = (Player) event.getEntity();
 			SheepWar instance = SheepWar.getInstance();
 			if(instance._playerInArena.containsKey(victim)) {
+				if(damagerEntity instanceof Player) {
+					Player damager = (Player) damagerEntity;
+					if(!instance._playerInArena.containsKey(damager)) {
+						event.setCancelled(true);
+						return;
+					}
+				}
+				Arena a = instance._arenaList.get(instance._playerInArena.get(victim));
+				if(a._playerInLobby.contains(victim)) {
+					event.setCancelled(true);
+					return;
+				}
+				if(victim.getHealth() <= event.getDamage()) {
+					a.eliminate(victim);
+					event.setCancelled(true);
+					if(damagerEntity instanceof Arrow) {
+						Player damager = (Player) ((Arrow) damagerEntity).getShooter();
+						for(Player player : a._playerInArena.keySet()) {
+							player.sendMessage(victim.getName() + " was killed by an arrow shot from " + damager.getName());
+						}
+					} else if(damagerEntity instanceof Snowball) {
+						Player damager = (Player) ((Snowball) damagerEntity).getShooter();
+						for(Player player : a._playerInArena.keySet()) {
+							player.sendMessage(victim.getName() + " was kileld by a snowball shot from " + damager.getName());
+						}
+					} else if(damagerEntity instanceof Egg) {
+						Player damager = (Player) ((Egg) damagerEntity).getShooter();
+						for(Player player : a._playerInArena.keySet()) {
+							player.sendMessage(victim.getName() + " was kileld by a egg shot from " + damager.getName());
+						}
+					}else {
+						for(Player player : a._playerInArena.keySet()) {
+							player.sendMessage(victim.getName() + " was killed by " + damagerEntity.getName());
+						}
+					}
+					
+				}
 				
 			}
 		}
