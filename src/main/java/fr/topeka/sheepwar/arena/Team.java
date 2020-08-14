d9a458836647d86579f5894662be00404b0e8ea2
Quentin Legot <quentin3157.github@gmail.com>
2020-08-14 23:47:36 +0200

    Fixed teleportation issue when game finish + added armor to players

--- src/main/java/fr/topeka/sheepwar/arena/Team.java
+++ src/main/java/fr/topeka/sheepwar/arena/Team.java
@@ -3,19 +3,35 @@ package fr.topeka.sheepwar.arena;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.Color;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
 
 public class Team {
 
 	public List<Player> _players = new ArrayList<>();
 	public List<Player> _playersAlive = new ArrayList<>();
 	public List<SpawnLocation> spawns = new ArrayList<>();
+	public ItemStack[] armor = new ItemStack[4];
 	public String _name;
 	private Arena linkedArena;
 	
-	public Team(String name, Arena arena) {
+	private static Material[] armorItem = { Material.LEATHER_BOOTS, Material.LEATHER_LEGGINGS, Material.LEATHER_CHESTPLATE, Material.LEATHER_HELMET };
+	
+	public Team(String name, Color color,  Arena arena) {
 		_name = name;
+		int i = 0;
+		for(Material material : armorItem) {
+			ItemStack item = new ItemStack(material);
+			LeatherArmorMeta itemMeta = ((LeatherArmorMeta) item.getItemMeta());
+			itemMeta.setColor(color);
+			item.setItemMeta(itemMeta);
+			armor[i] = item;
+			i++;
+		}
 		linkedArena = arena;
 	}
 	
